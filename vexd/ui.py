8e160a06e4ff2bd4ff19ec2d07ad387180fdab7e
Phillip Dexheimer <phillip.dexheimer@cchmc.org>
2022-10-05 14:22:58 -0400

    Made Gene Search the first page, reorganized {ui,api}.py

--- vexd/ui.py
+++ vexd/ui.py
@@ -1,65 +1,85 @@
 import base64
 from collections import Counter
-from flask import Blueprint, current_app, render_template, send_from_directory
-from flask.helpers import make_response
 import re
+
+from flask import Blueprint, current_app, redirect, render_template, send_from_directory, url_for
+from flask.helpers import make_response
+import pandas as pd
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs
-import pandas as pd
-from .geo import geo
+
 from . import plot
+from .geo import geo
 from .utils import enrichment, parse_dl_files
 
 bp = Blueprint('ui', __name__, url_prefix='/')
 
+#########################################
+## Top level pages
+#########################################
+
+# Home page
 @bp.route('/')
 def vexd_home():
+    return redirect(url_for('ui.gene_search'))
+
+# Genes
+@bp.route('/gene_lookup')
+def gene_search():
+    return render_template('gene_search.html')
+
+# Enrichment
+@bp.route('/enrich')
+def multiple_genes():
+    return render_template('multiple_genes.html')
+
+# Studies
+@bp.route('/study')
+def study_search():
     return render_template(
-        'homepage.html',
+        'study_search.html',
         study_count=geo().num_studies(),
         virus_count=geo().num_viruses()
     )
 
-@bp.route('/virus')
-@use_kwargs({'q': fields.Str()}, location='query')
-@use_kwargs({'tissue': fields.Str()}, location='query')
-@use_kwargs({'descendants': fields.Bool(missing=False)}, location='query')
-def virus_results(q, tissue, descendants):
-    return render_template(
-        'combined_results.html', 
-        virus=q, 
-        tissue=tissue,
-        search_kids=descendants,
-        search_results=geo().search_studies(q, tissue, descendants)
+# API
+@bp.route('/api')
+def api_docs():
+    return render_template('api_docs.html')
+
+# Downloads
+@bp.route('/downloads')
+def download_page():
+    return render_template('downloads.html', 
+        file_info=parse_dl_files(current_app.config['DOWNLOAD_DIRECTORY'])
     )
 
-@bp.route('/virus/txt')
-@use_kwargs({'q': fields.Str()}, location='query')
-@use_kwargs({'tissue': fields.Str()}, location='query')
-@use_kwargs({'descendants': fields.Bool(missing=False)}, location='query')
-def virus_results_as_text(q, tissue, descendants):
-    response = make_response(render_template(
-        'combined_results.txt', 
-        virus=q, 
-        tissue=tissue,
-        search_kids=descendants,
-        search_results=geo().search_studies(q, tissue, descendants)
-    ))
-    response.headers.set('Content-disposition', 'attachment', filename="studies.txt")
-    response.content_type = 'text/plain'
-    return response
+# Methods
+@bp.route('/methods')
+def methods():
+    return render_template('methods.html')
 
-@bp.route('/geo/', defaults={'gse_id': None})
-@bp.route('/geo/<gse_id>')
-@use_kwargs({'gse_id': fields.Str()}, location='query')
-def display_geo_entry(gse_id):
-    return render_template('gse.html', gse=geo().get_gse_info(gse_id))
+# About VExD
+@bp.route('/about')
+def about():
+    return render_template('about.html', virus_counts=geo().count_by_virus())
 
+# Help
+@bp.route('/help')
+def help():
+    return render_template('help.html')
+
+#########################################
+## (Single) Gene-related pages
+#########################################
+
+# Results page
 @bp.route('/gene')
 @use_kwargs({'q': fields.Str()}, location='query')
 def display_gene_info(q):
     return render_template('gene.html', gene=geo().get_gene_info(q), gene_results=geo().get_results_by_gene(q))
 
+# Boxplot figure
 @bp.route('/gene_plot')
 @use_kwargs({'q': fields.Str()}, location='query')
 def gene_boxplot(q):
@@ -72,6 +92,25 @@ def gene_boxplot(q):
     response.content_type = 'image/png'
     return response
 
+# Gene Results as text (for download)
+@bp.route('/gene/txt')
+@use_kwargs({'q': fields.Str()}, location='query')
+def gene_info_text(q):
+    gene_info = geo().get_gene_info(q)
+    response = make_response(render_template(
+        'gene_results.txt', 
+        gene=gene_info, 
+        gene_results=geo().get_results_by_gene(q)
+    ))
+    response.headers.set('Content-disposition', 'attachment', filename=f"{gene_info['symbol']}_results.txt")
+    response.content_type = 'text/plain'
+    return response
+
+#########################################
+## (Multiple Gene) Enrichment-related pages
+#########################################
+
+# Heatmap Generation/Statistics Return
 @bp.route('/gene/multiple', methods=['POST'])
 @use_kwargs({
     'raw_genes': fields.Str(), 
@@ -114,27 +153,25 @@ def gene_heatmap(raw_genes, remove_unknown, sort_genes):
         'heatmap': base64.b64encode(plot.gene_heatmap(gene_list, gene_results, sort_genes)).decode(),
     })
 
-@bp.route('/enrich')
-def multiple_genes():
-    return render_template('multiple_genes.html')
+#########################################
+## Study-related pages
+#########################################
 
-@bp.route('/gene/txt')
+# Search Results
+@bp.route('/virus')
 @use_kwargs({'q': fields.Str()}, location='query')
-def gene_info_text(q):
-    gene_info = geo().get_gene_info(q)
-    response = make_response(render_template(
-        'gene_results.txt', 
-        gene=gene_info, 
-        gene_results=geo().get_results_by_gene(q)
-    ))
-    response.headers.set('Content-disposition', 'attachment', filename=f"{gene_info['symbol']}_results.txt")
-    response.content_type = 'text/plain'
-    return response
+@use_kwargs({'tissue': fields.Str()}, location='query')
+@use_kwargs({'descendants': fields.Bool(missing=False)}, location='query')
+def virus_results(q, tissue, descendants):
+    return render_template(
+        'combined_results.html', 
+        virus=q, 
+        tissue=tissue,
+        search_kids=descendants,
+        search_results=geo().search_studies(q, tissue, descendants)
+    )
 
-@bp.route('/gene_lookup')
-def gene_search():
-    return render_template('gene_search.html')
-
+# Download single-study results
 result_args = {
     'virus': fields.Str(),
     'study': fields.Str(),
@@ -151,16 +188,35 @@ def download_results(virus, study, platform, cell_type, geneSet):
     response.content_type = 'text/plain'
     return response
 
-@bp.route('/methods')
-def methods():
-    return render_template('methods.html')
+# Search Results as text (for download)
+@bp.route('/virus/txt')
+@use_kwargs({'q': fields.Str()}, location='query')
+@use_kwargs({'tissue': fields.Str()}, location='query')
+@use_kwargs({'descendants': fields.Bool(missing=False)}, location='query')
+def virus_results_as_text(q, tissue, descendants):
+    response = make_response(render_template(
+        'combined_results.txt', 
+        virus=q, 
+        tissue=tissue,
+        search_kids=descendants,
+        search_results=geo().search_studies(q, tissue, descendants)
+    ))
+    response.headers.set('Content-disposition', 'attachment', filename="studies.txt")
+    response.content_type = 'text/plain'
+    return response
 
-@bp.route('/downloads')
-def download_page():
-    return render_template('downloads.html', 
-        file_info=parse_dl_files(current_app.config['DOWNLOAD_DIRECTORY'])
-    )
+# Single GEO Entry page
+@bp.route('/geo/', defaults={'gse_id': None})
+@bp.route('/geo/<gse_id>')
+@use_kwargs({'gse_id': fields.Str()}, location='query')
+def display_geo_entry(gse_id):
+    return render_template('gse.html', gse=geo().get_gse_info(gse_id))
 
+#########################################
+## Download-related pages
+#########################################
+
+# Individual file download
 @bp.route('/download/<filename>')
 @use_kwargs({'filename': fields.Str()}, location='view_args')
 def download_file(filename):
@@ -169,15 +225,3 @@ def download_file(filename):
         filename,
         as_attachment=True
     )
-
-@bp.route('/api')
-def api_docs():
-    return render_template('api_docs.html')
-
-@bp.route('/about')
-def about():
-    return render_template('about.html', virus_counts=geo().count_by_virus())
-
-@bp.route('/help')
-def help():
-    return render_template('help.html')
